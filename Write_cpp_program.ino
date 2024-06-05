f0c3130082fc1d4eaf0a775e26199e0def8d21a5
Emil <emilhalvorsen@proton.me>
2024-06-05 11:06:10 +0200

    Create Write_cpp_program.ino

---
+++ Write_cpp_program.ino
@@ -0,0 +1,35 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.delay(50);
+    DigiKeyboard.sendKeyStroke(KEY_ESC, MOD_CONTROL_LEFT);
+    DigiKeyboard.delay(750);
+    DigiKeyboard.print("Visual Studio Code");
+    DigiKeyboard.delay(500);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1500);
+    DigiKeyboard.sendKeyStroke(KEY_N, MOD_CONTROL_LEFT);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.print("#include <iostream>");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.print("");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.print("int main() {");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.print("    std::cout << \"Hello, World!\" << std::endl;");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.print("    return 0;");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.sendKeyStroke(KEY_S, MOD_CONTROL_LEFT);
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.print("HelloWorld.cpp")
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+}
+
+void loop() {}
